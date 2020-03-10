ea623d63b13ff0385fe3d2d9638bf2742a62dc10
davelovellCARU <david.lovell@churcharmy.org>
2020-03-10 14:27:59 +0000

    fixing 'carultools::'

--- R/functions.R
+++ R/functions.R
@@ -17,13 +17,13 @@ ca_all_cols <- function(){c(carutools::ca_secondary_cols(),
                             carutools::ca_tertiary_cols())}
 
 ## Return individual Colours ----------------------------------------------
-ca_orange <- function()   {carultools::ca_allcols()[["orange"]]}
-ca_lime <- function()     {carultools::ca_allcols()[["lime"]]}
-ca_gold <- function()     {carultools::ca_allcols()[["gold"]]}
-ca_purple <- function()   {carultools::ca_allcols()[["purple"]]}
-ca_green <- function()    {carultools::ca_allcols()[["green"]]}
-ca_maroon <- function()   {carultools::ca_allcols()[["maroon"]]}
-ca_cyan <- function()     {carultools::ca_allcols()[["cyan"]]}
-ca_lightteal <- function(){carultools::ca_allcols()[["lightteal"]]}
-ca_brown <- function()    {carultools::ca_allcols()[["brown"]]}
-ca_darkteal <- function() {carultools::ca_allcols()[["darkteal"]]}
+ca_orange <- function()   {carutools::ca_allcols()[["orange"]]}
+ca_lime <- function()     {carutools::ca_allcols()[["lime"]]}
+ca_gold <- function()     {carutools::ca_allcols()[["gold"]]}
+ca_purple <- function()   {carutools::ca_allcols()[["purple"]]}
+ca_green <- function()    {carutools::ca_allcols()[["green"]]}
+ca_maroon <- function()   {carutools::ca_allcols()[["maroon"]]}
+ca_cyan <- function()     {carutools::ca_allcols()[["cyan"]]}
+ca_lightteal <- function(){carutools::ca_allcols()[["lightteal"]]}
+ca_brown <- function()    {carutools::ca_allcols()[["brown"]]}
+ca_darkteal <- function() {carutools::ca_allcols()[["darkteal"]]}
