d8964f60e56cce45324a43db93b70d86da2f72a0
davelovellCARU <david.lovell@churcharmy.org>
2020-04-16 11:03:44 +0100

    new function ct_is_postcode

--- R/functions.R
+++ R/functions.R
@@ -59,3 +59,4 @@ ct_postcode_regex <- function(anchors = TRUE){
   else "^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$"
 }
 
+ct_is_postcode <- function(vector) stringr::str_detect(vector, carutools::ct_postcode_regex(anchors = TRUE))
